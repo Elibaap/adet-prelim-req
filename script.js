053940e3b47c81a76197a0bbff634766f11931a6
Daddy Gi <91370424+daddygi@users.noreply.github.com>
2022-07-02 20:42:58 +0800

    chore: optimize js (#1)
    * chore: optimize js
    
    * chore: updated

--- script.js
+++ script.js
@@ -1,51 +1,29 @@
-const animation1 = document.querySelector('.animation1');
-const animation2 = document.querySelector('.animation2');
-const animation3 = document.querySelector('.animation3');
-const animation4 = document.querySelector('.animation4');
-const animation5 = document.querySelector('.animation5');
 
-const btn1 = document.querySelector('#btn1');
-const btn2 = document.querySelector('#btn2');
-const btn3 = document.querySelector('#btn3');
-const btn4 = document.querySelector('#btn4');
-const btn5 = document.querySelector('#btn5');
+const animation = document.querySelectorAll('.animation');
+
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+const btn3 = document.querySelector('.btn3');
+const btn4 = document.querySelector('.btn4');
+const btn5 = document.querySelector('.btn5');
 
 const reset = () => {
-  animation1.classList.add('hide');
-  animation2.classList.add('hide');
-  animation3.classList.add('hide');
-  animation4.classList.add('hide');
-  animation5.classList.add('hide');
-};
+  animation.forEach(query => {
+    query.classList.add('hide');
+  })
+}
 
 reset();
 
-btn1.addEventListener('click', () => {
-  reset();
-  animation1.classList.toggle('hide');
-});
+function animate(btn, anm){
+  btn.addEventListener('click', () => {
+    reset();
+    anm.classList.toggle('hide');
+  });
+}
 
-btn2.addEventListener('click', () => {
-  reset();
-  animation2.classList.toggle('hide');
-});
-
-btn3.addEventListener('click', () => {
-  reset();
-  animation3.classList.toggle('hide');
-});
-
-btn4.addEventListener('click', () => {
-  reset();
-  animation4.classList.toggle('hide');
-});
-
-btn5.addEventListener('click', () => {
-  reset();
-  animation5.classList.toggle('hide');
-});
-
-/**
- * TODO gian:
- * xamsxa msd smasd qwe qwej
- */
+animate(btn1, animation1);
+animate(btn2, animation2);
+animate(btn3, animation3);
+animate(btn4, animation4);
+animate(btn5, animation5);
